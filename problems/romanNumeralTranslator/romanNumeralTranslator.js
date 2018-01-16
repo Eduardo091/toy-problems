127f9eedc955cb03d5d91ecf74829b74749cfe44
Eduardo091 <urrutia.e14@gmail.com>
2018-01-16 10:13:01 -0600

    toy problems

--- problems/romanNumeralTranslator/romanNumeralTranslator.js
+++ problems/romanNumeralTranslator/romanNumeralTranslator.js
@@ -26,8 +26,36 @@ var DIGIT_VALUES = {
   M: 1000
 };
 
+
+
 var translateRomanNumeral = function(romanNumeral) {
 // TODO: Implement me!
 /* START SOLUTION */
   /* END SOLUTION */
+//input roman numberal
+//output numbera
+// store a number
+// if the input is invalid return null
+//if the numeral  is before the larger one subtract it
+//create a path to digital DIGIT_VALUES
+//make a var for  DIGIT_VALUES
+var num = 0;
+
+translateRomanNumeral.prototype =Object.create(DIGIT_VALUES.prototype)
+
+for(var i = 0; i < romanNumerale.length; i++){
+for(var key in this.DIGIT_VALUES){
+
+  if(romanNumeral[0] < romanNumeral[1]){
+  num = romanNumeral[1]-romanNumeral[0];
+  return num;
+  }
+else if(romanNumerla[0] > romanNumeral[1]){
+   num = romanNumeral;
+   return num;
+}else{
+  return null;
+}
+}
+}
 };
